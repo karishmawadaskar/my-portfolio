db3a812f1310d1ef0fbcf5df5219bb8b5e9ce233
karishmawadaskar <karishmawadaskar@gmail.com>
2025-07-06 17:02:57 +0530

    projects updated

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const projects = [
   {
-    title: "Calculator",
-    description: "A basic calculator for simple arithmetic operations.",
-    link: "#",
+    title: "fuji-film ",
+    description: "The Fuji Film  is a full-stack web application developed using the MERN stack, designed to efficiently manage film records. The system allows users to add, view, edit, and delete film entries, making it easier to track and organize film-related data.",
+    link: "https://fuji-film2.onrender.com",
   },
   {
-    title: "Login Form",
-    description: "Simple login form with validation.",
-    link: "#",
+    title: "QuickNotes [Front End Project] ",
+    description: "Quick Notes, a simple yet powerful web application built using React.js. Whether you're a student juggling assignments or a professional managing a busy schedule, this app is designed to help you stay on top of your tasks! 💻📚",
+    link: "https://lnkd.in/gtE-EjbT",
   },
   {
-    title: "To-Do App",
-    description: "Track your tasks with a React-based to-do list.",
-    link: "#",
+    title: "Kids Quizzland [Front End Project]",
+    description: "Kids Quizzland is a vibrant and engaging platform where kids can explore and learn through quizzes. Our goal is to make learning enjoyable by providing quizzes in a simple and funny language, accompanied by fun stickers and images.This is a very interactive layout of our website.",
+    link: "https://kids-quizzland.netlify.app",
   },
   {
-    title: "Weather App",
-    description: "Check current weather using an open API.",
-    link: "#",
+    title: "Agrimart [Front End Project]",
+    description: "Agrimart - where we deliver fresh, organic, and wholesome products directly to your doorstep. At Agrimart, we believe in bringing the goodness of nature straight from farms to your table. With our easy-to-use platform, you can explore a wide range of farm-fresh products and enjoy the convenience of quick delivery.",
+    link: "https://agrimart-eta.vercel.app/",
   },
 ];
 
